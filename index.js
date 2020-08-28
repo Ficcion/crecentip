067f94ea9119e57ca0828255cda0958541019ac2
Al Ferreyra <al@iMac-de-Al.local>
2020-08-28 13:09:19 -0500

    Control de SPA

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -41,6 +42,10 @@ app.use( '/archivos', require('./routes/archivos') );
 app.use( '/buscar', require('./routes/buscar') );
 app.use( '/login', require('./routes/auth') );
 
+// Control de rutas en Angular
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+});
 
 app.listen( process.env.PORT, () => {
     console.log( 'Servidor corriendo en puerto ' + process.env.PORT );
